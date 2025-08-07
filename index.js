04e2c5bbe5f85b5ebcd4ce591bcbb3c8a02eccb4
Rodri0701 <rodrigoesparza117@gmail.com>
2025-08-07 17:23:28 -0600

    Fuccionalidad para renderizar el DOM y al mismo tiempo cargar imagenes mediante un for

--- index.js
+++ index.js
@@ -4,3 +4,42 @@ console.log(
   'font-size: 40px; font-weight: bold; color: #8A2BE2; font-family: "Comic Sans MS", cursive, sans-serif;'
 );
 console.log("Check out more projects at https://bigdevsoon.me");
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+  const contenedor = document.getElementById("contenedor-imagenes");
+const botones = document.querySelectorAll("button[data-categoria]");
+
+botones.forEach((btn) => {
+  btn.addEventListener("click", function()  {
+    const categoria = btn.dataset.categoria;
+console.log (categoria)
+
+
+
+    // Limpiamos el contenedor
+    contenedor.innerHTML = "";
+
+    // Cargamos 10 imágenes por categoría
+   for (let i = 1; i <= 50; i++) {
+  const img = new Image(); // crea elemento imagen
+  img.src = `./assets/character-images-left-side/${categoria}/${i}.png`;
+  img.alt = `${categoria} ${i}`;
+  img.className = "rounded-xl shadow-lg w-40 h-40 object-cover mb-4";
+
+  img.onload = () => {
+    // Solo agrega la imagen si carga bien
+    contenedor.appendChild(img);
+  };
+
+  img.onerror = () => {
+    // imagen no existe
+  
+  };
+}
+  });
+});
+});
+
+
